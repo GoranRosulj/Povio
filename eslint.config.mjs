b541917f798b84875f7e0b5ce6a07999051415d0
GoranRosulj <goran.rosulj@gmail.com>
2025-01-31 03:20:27 +0100

    Initialization of playwright project

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import playwright from 'eslint-plugin-playwright';
+
+export default [
+    {
+        files: ['**/*.{js,mjs,cjs,ts,tsx}'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+            parser: parser,
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            'prettier': prettier,
+            'playwright': playwright,
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            ...tseslint.configs.recommended.rules,
+            ...playwright.configs.recommended.rules,
+            '@typescript-eslint/no-unused-vars': ['warn', { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': true }],
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
+            'indent': ['error', 4],
+            'no-trailing-spaces': 'error',
+            'eol-last': ['error', 'always'],
+            'curly': ['error', 'all'],
+        },
+    },
+    {
+        files: [''],
+        rules: {
+            // Disable no-wait-for-timeout
+            'playwright/no-wait-for-timeout': 'off',
+        },
+    },
+];
